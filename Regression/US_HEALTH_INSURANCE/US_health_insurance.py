071c94b2aeef39e765ced3f4d56d612cbde3617c
Sreedev <sreedev.r5@gmail.com>
2020-05-24 23:14:17 +0530

    Got basic out put with a RMSE of 17938.58625704702, Need to get better

--- Regression/US_HEALTH_INSURANCE/US_health_insurance.py
+++ Regression/US_HEALTH_INSURANCE/US_health_insurance.py
@@ -9,9 +9,10 @@ Model to predict the charges of insurance from BMI.
 """
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import numpy as np
 
 # Importing the dataset
 dataset = pd.read_csv('insurance.csv')
@@ -20,11 +21,21 @@ y = dataset.iloc[:, 6].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/2, random_state = 0)
 
 
 X_train = X_train.reshape(1,-1) 
 y_train = y_train.reshape(1,-1) 
+X_test = X_test.reshape(1,-1) 
+y_test = y_test.reshape(1,-1)
+
+"""# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -33,3 +44,16 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+
+# RMSE
+print(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('BMI vs Insurance charges (Test set)')
+plt.xlabel('BMI')
+plt.ylabel('Insurance charge')
+plt.show()
